717a7cbce0e8ec71a272cab4c58efd0c5be45129
Marcus Recto <mdrecto@ualberta.ca>
2026-05-11 13:59:39 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo for going through build your own lisp
---
+++ hello_world.c
@@ -0,0 +1,7 @@
+// I have done this before
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  puts("Hello, world!");
+  return 0;
+}
